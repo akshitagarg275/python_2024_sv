46288b6ae163ed81d2cd4ef9cd5f1db08bbe1f27
akshitagarg275 <akshitagarg275@gmail.com>
2024-02-29 09:34:41 +0530

    sets and dictionary

--- 02data_structures/04sets.py
+++ 02data_structures/04sets.py
@@ -3,7 +3,7 @@
 # indexing is not there
 # there is no fixed position
 
-a = {1,2,3,4,5,2,1,2}
+# a = {1,2,3,4,5,2,1,2}
 # print(a)
 # print(type(a))
 
@@ -11,3 +11,37 @@ a = {1,2,3,4,5,2,1,2}
 #     print(i)
 
 # print(len(a))
+
+
+s = {12,34,56,78,67}
+# print("BEFORE: ", s)
+# s.add(11)
+# print("AFTER: ", s)
+
+# s.remove(12)
+
+# TODO: it will throw an error if the element donot exist
+# s.remove(2)
+
+# s.discard(12)
+# s.discard(2)
+
+# print("BEFORE: ", s)
+# s.pop()
+# print("AFTER: ", s)
+# print(s)
+
+a = {1,2,3,4,5}
+b = {4,5,6,7}
+
+# print(a.union(b))
+# print(a|b)
+
+# print(a.intersection(b))
+# print(a&b)
+
+# print(a.difference(b))
+# print(a-b)
+
+# print(b.difference(a))
+# print(b-a)
---
+++ 02data_structures/05dict.py
@@ -0,0 +1,62 @@
+# Dict -> { key : value } , dict ()
+# keys -> immutable datatypes int, str, tuple
+# mutable , iterable 
+# to access any particular value we use the keys
+
+stu = {
+    "fname" : "John",
+    "lname" : "Doe", 
+    "age" : 23,
+    "courses" : ["Python", "Jasvascript"],
+    "isEnrolled": True,
+    "fees" : 234.23
+}
+
+# print(stu.items())
+
+# for k,v in stu.items():
+#     print(f"key is : {k} and value is: {v}")
+
+# print(stu)
+# print(type(stu))
+
+# print("age: " ,stu["age"])
+# print(stu["courses"][0])
+
+# print(stu.keys())
+# print(stu.values())
+# print(stu.items())
+
+# print("BEFORE: ", stu)
+# print(stu.pop("age1"))
+# print("AFTER: ", stu)
+
+# print(stu.get("fname1", 'NA'))
+
+# print(stu.popitem())
+
+# d = dict(name = "Ram", course = "python")
+# print(d)
+
+
+# d2 = dict(id = 123, **stu)
+# d2 = dict(**stu, id = 123)
+# print(d2)
+
+
+word = "Python Programming"
+# keep a count of each char in the string
+
+freq = {}
+
+for ch in word:
+    # if key already exists
+    if (freq.get(ch) != None):
+        freq[ch] = freq[ch] + 1
+
+    else:
+        # first time
+        freq[ch] = 1
+
+print(freq)
+
